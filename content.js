a64ccf74e9e8e1d59f9f4d74626f1c219191a01b
tweimann <t@tweimann.xyz>
2022-08-24 15:33:55 +0200

    prototyp

---
+++ content.js
@@ -0,0 +1,31 @@
+var selection = Math.floor(Math.random() * 9) + 1
+
+if (selection == 1) {
+    tag = '<video controls autoplay loop class="centered" title="quelle: https://www.reddit.com/r/600euro/comments/wvhjsu/was_attila_nicht_erw%C3%A4hnt_breuer_impft_pers%C3%B6nlich/" src="./src/1.mp4"></video>'
+}
+else if (selection == 2) {
+    tag = '<video controls autoplay loop class="centered" title="quelle: https://www.youtube.com/watch?v=N4Qy8MqA4qw" src="./src/2.mp4"></video>'
+}
+else if (selection == 3) {
+    tag = '<img class="centered" title="quelle: https://kritisch-denken.ch/wp-content/uploads/2020/12/ken_jebsen_joker.jpg" src="./src/3.jpg"></img>'
+}
+else if (selection == 4) {
+    tag = ''
+}
+else if (selection == 5) {
+    tag = ''
+}
+else if (selection == 6) {
+    tag = ''
+}
+else if (selection == 7) {
+    tag = ''
+}
+else if (selection == 8) {
+    tag = ''
+}
+else if (selection == 9) {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_self');
+}
+
+document.write(tag)
